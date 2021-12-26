ebc41264920dca20d00dec4b3009d118e2da3ddc
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2021-12-26 12:59:21 +0700

    chore: add csp configuration non-strict

--- next.config.js
+++ next.config.js
@@ -1,4 +1,58 @@
 /** @type {import('next').NextConfig} */
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-inline';
+  img-src 'self' blob: data: https:;
+  style-src 'self' 'unsafe-inline';
+  connect-src 'self';
+  object-src 'none';
+  frame-src 'none';
+`;
+
+securityHeader = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "geolocation=(), interest-cohort=()",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\n/g, ""),
+  },
+];
+
 module.exports = {
   reactStrictMode: true,
+  headers: async () => {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeader,
+      },
+    ];
+  },
 };
